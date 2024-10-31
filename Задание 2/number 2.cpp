5862427e0a0ccc7458e8752fa317e09a9d8b0417
MrIceRam <skyfarliv@gmail.com>
2024-10-31 20:22:00 +0300

    linmin

---
+++ Задание 2/number 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <cmath>
+int main{
+    double a, x; 
+    std::cout << "a = "; 
+    std::cin >> a; 
+    std::cout << "x = "; 
+    std::cin >> x; 
+    if (x == 0) { 
+        std::cout << "ошибка x == 0"; 
+    } 
+    else if (abs(x) < 1) { 
+        std::cout << a * log(abs(x)); 
+    } 
+    else if (a - pow(x, 2) < 0) { 
+        std::cout << "ошибка отрицательное число"; 
+    } 
+    else { 
+        std::cout << sqrt(a - pow(x, 2)); 
+    }
+}
