3f108fe9b6120a6473eb565c0f4cadb6180d5de6
MrIceRam <skyfarliv@gmail.com>
2024-11-14 20:44:25 +0300

    Num4_7-9fix

--- Задание 4/number 8.cpp
+++ Задание 4/number 8.cpp
@@ -1,92 +1,92 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 using namespace std;
-
-
-int char_to_int(char symbol) {
-	switch (symbol) {
-	case '0': return 0; break;
-	case '1': return 1; break;
-	case '2': return 2; break;
-	case '3': return 3; break;
-	case '4': return 4; break;
-	case '5': return 5; break;
-	case '6': return 6; break;
-	case '7': return 7; break;
-	case '8': return 8; break;
-	case '9': return 9; break;
-	case 'A': return 10; break;
-	case 'B': return 11; break;
-	case 'C': return 12; break;
-	case 'D': return 13; break;
-	case 'E': return 14; break;
-	case 'F': return 15; break;
-	}
-	return -1;
-}
-
-
-string int_to_char(int symbol) {
-	switch (symbol) {
-	case 0: return "0"; break;
-	case 1: return "1"; break;
-	case 2: return "2"; break;
-	case 3: return "3"; break;
-	case 4: return "4"; break;
-	case 5: return "5"; break;
-	case 6: return "6"; break;
-	case 7: return "7"; break;
-	case 8: return "8"; break;
-	case 9: return "9"; break;
-	case 10: return "A"; break;
-	case 11: return "B"; break;
-	case 12: return "C"; break;
-	case 13: return "D"; break;
-	case 14: return "E"; break;
-	case 15: return "F"; break;
-	}
-	return "";
-}
-
-
-double funTenCC(string line, int base_num = 2) { // перевод в 10 с.с.
-	double new_x = 0; // полученное число в 10 с.с.
-
-	for (int i = 0; i < line.size(); i++) { // пробегание по всем цифрам
-		int symbol = char_to_int(line[i]); // конвертированный символ в цифру
-		new_x += symbol * pow(base_num, line.size() - i - 1); // запись в число цифры line[i]
-	}
-
-	return new_x;
-}
-
-
-string funNewCC(int num, int base_num = 2) { // перевод в нужную с.с.
-	string new_x = int_to_char(num % base_num);
-
-	if (num > 0) {
-		return funNewCC(num / base_num) + new_x;
-	}
-
-	new_x = "";
-	return new_x;
-}
-
 
 int main()
 {
-	string x;
-	int old_ss, new_ss;
-	cout << "Number: ";
-	cin >> x;
-	cout << "Old CC: ";
-	cin >> old_ss;
-	cout << "New CC: ";
-	cin >> new_ss;
+    double matrix_a[3][4] = { // 3x4
+        {5, 2, 0, 10},
+        {3, 5, 2, 5},
+        {20, 0, 0, 0}};
+                
+    double matrix_b[4][2] = { // 4x2
+        {1.20, 0.50},
+        {2.80, 0.40},
+        {5.00, 1.00},
+        {2.00, 1.50}};
+        
+    double matrix_c[3][2]; // 3x2
 
-	int num = funTenCC(x, old_ss);
-	cout << "Number: " << num << endl;
-	cout << "Number: " << funNewCC(num, new_ss) << endl;
-	return 0;
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 2; y++) {
+            for (int i = 0; i < 4; i++) {
+                matrix_c[x][y] += matrix_a[x][i] * matrix_b[i][y];
+            }
+        }
+    }
+
+    // Вывод матрицы C
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 2; y++) {
+            cout << matrix_c[x][y] << "               ";
+        }
+        cout << endl;
+    }
+    cout << "------------------------------\n";
+    
+	//////////////////////////////////////////////////////// Деньги
+    int maxSeller = 0;
+	int minSeller = 0;
+    double maxMoney = 0;
+	double minMoney = 10000000;
+    for (int x = 0; x < 3; x++) {
+        if (matrix_c[x][0] > maxMoney) {
+			maxSeller = x + 1;
+			maxMoney = matrix_c[x][0];
+		}
+        if (matrix_c[x][0] < minMoney) {
+			minSeller = x + 1;
+			minMoney = matrix_c[x][0]; 
+		}
+    }
+    cout << "Seller " << maxSeller << " MaxMoney: " << maxMoney << endl;
+    cout << "Seller " << minSeller << " MinMoney: " << minMoney << endl;
+    
+	//////////////////////////////////////////////////////// Комиссионные
+    maxSeller = 0;
+	minSeller = 0;
+    double maxComss = 0;
+	double minComss = 10000000;
+    for (int x = 0; x < 3; x++) {
+        if (matrix_c[x][1] > maxComss) {
+			maxSeller = x + 1;
+			maxComss = matrix_c[x][1]; 
+		}
+        if (matrix_c[x][1] < minComss) {
+			minSeller = x + 1;
+			minComss = matrix_c[x][1]; 
+		}
+    }
+    cout << "Seller " << maxSeller << " MaxComss " << maxComss << endl;
+    cout << "Seller " << minSeller << " MinComss " << minComss << endl;
+
+	//////////////////////////////////////////////////////// Общая сумма денег
+    double sumMoney = 0;
+    for (int x = 0; x < 3; x++) {
+    	sumMoney += matrix_c[x][0];
+	}
+    cout << "SumMoney = " << sumMoney << endl;
+    
+	/////////////////////////////////////////////////////// Общая сумма комиссионных
+    double sumComss = 0;
+    for (int x = 0; x < 3; x++) {
+    	sumComss += matrix_c[x][1];
+	}
+    cout << "SumComss = " << sumComss << endl;
+
+	/////////////////////////////////////////////////////// Общая сумма денег (+ комиссионные)
+    double SumMoneyResult = 0;
+    for (int x = 0; x < 3; x++) {
+    	SumMoneyResult += matrix_c[x][0] + matrix_c[x][1];
+	}
+    cout << "SumMoneyResult = " << SumMoneyResult << endl;
 }
